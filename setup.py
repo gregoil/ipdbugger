649bbec7d55415e12e24fd138fed59f3f535ed1b
Osher De Paz <osherdepaz@gmail.com>
2018-06-22 13:51:56 +0300

    Run tests on Python 3.5 as well

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """Setup file for handling packaging and distribution."""
 from setuptools import setup
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 setup(
     name="ipdbugger",
@@ -17,14 +17,20 @@ setup(
                       "future",
                       "colorama",
                       "termcolor"],
+    extras_require={
+        "dev": ["flake8", "pylint",
+                "pytest", "pytest-cov",
+                "mock"]
+    },
     packages=["ipdbugger"],
-    python_requires=">=2.7",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
     package_data={'': ['*.xls', '*.xsd', '*.json',
                        '*.css', '*.xml', '*.rst']},
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
         'Programming Language :: Python :: 3.6'
     ],
     zip_safe=False
